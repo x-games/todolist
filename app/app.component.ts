2454a53bd09a69952b405714fd4b2cbfbd7ae06c
x-games <wlad.ruban@gmail.com>
2016-09-28 22:24:03 +0300

    components

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,18 +1,22 @@
 import { Component } from "@angular/core";
+import { User } from "./shared/user/user";
 
 @Component({
   selector: "my-app",
-  template: `
-    <StackLayout>
-      <Image src="res://logo_login" stretch="none" horizontalAlignment="center"></Image>
-      <TextField hint="Email Address" keyboardType="email"
-        autocorrect="false" autocapitalizationType="none"></TextField>
-      <TextField hint="Password" secure="true"></TextField>
-  
-      <Button text="Sign in" class="submit-button"></Button>
-      <Button text="Sign up for Groceries"></Button>
-    </StackLayout>
-  `,
+  templateUrl: "pages/login/login.html",
   styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
-export class AppComponent {}
+export class AppComponent {
+  user: User;
+  isLoggingIn = true;
+
+  constructor() {
+    this.user = new User();
+  }
+  submit() {
+    alert("You’re using: " + this.user.email);
+  }
+  toggleDisplay() {
+    this.isLoggingIn = !this.isLoggingIn;
+  }
+}
