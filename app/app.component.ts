543e9f6cce6039df5c91106c8959a390383dcb9a
x-games <wlad.ruban@gmail.com>
2016-09-29 23:07:35 +0300

    sevices

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from "@angular/core";
 import { User } from "./shared/user/user";
+import { UserService } from "./shared/user/user.service";
 
 @Component({
   selector: "my-app",
+  providers: [UserService],
   templateUrl: "pages/login/login.html",
   styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
@@ -10,11 +12,30 @@ export class AppComponent {
   user: User;
   isLoggingIn = true;
 
-  constructor() {
+  constructor(private userService: UserService) {
     this.user = new User();
+    this.user.email = "user@nativescript.org";
+    this.user.password = "password";
   }
   submit() {
-    alert("You’re using: " + this.user.email);
+    if (this.isLoggingIn) {
+      this.login();
+    } else {
+      this.signUp();
+    }
+  }
+  login() {
+    // TODO: Define
+  }
+  signUp() {
+    this.userService.register(this.user)
+        .subscribe(
+            () => {
+              alert("Your account was successfully created.");
+              this.toggleDisplay();
+            },
+            () => alert("Unfortunately we were unable to create your account.")
+        );
   }
   toggleDisplay() {
     this.isLoggingIn = !this.isLoggingIn;
--- app/shared/config.ts
+++ app/shared/config.ts
@@ -1,4 +1,4 @@
 export class Config {
-  static apiUrl = "https://api.everlive.com/v1/GWfRtXi1Lwt4jcqK/";
+  static apiUrl = "https://api.everlive.com/v1/myod2aigroeie80o/";
   static token = "";
 }
